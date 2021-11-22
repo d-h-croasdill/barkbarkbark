bdc40a8d1ce3c6c59731053fe1750f536b0d08f9
Alessia Ianni-Palarchio <alessia@mipsscan.com>
2021-11-22 16:26:56 -0500

    Adding Audio!
    Testing an audio bit for character text boxes

--- spool/src/script.js
+++ spool/src/script.js
@@ -4,3 +4,18 @@
 //     var elem = document.getElementById('passage-container');
 //     elem.scrollTop = elem.scrollHeight;
 //   }, 5000);
+
+var textBoxNum = 0;
+var newLen = 0;
+
+var snd = new Audio("src/pickupCoin.wav");
+
+function playSFX(){
+  newLen = $("div.dialog").length;
+  if(newLen != textBoxNum){
+    textBoxNum = newLen;
+    snd.play();
+  }
+}
+
+setInterval(playSFX, 500);
