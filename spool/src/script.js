89881a417dc2f59dd3c2d9df3f9f9e47e717eedf
casper <yeah@yeah.com>
2021-11-22 15:12:38 -0500

    prototype progress

---
+++ spool/src/script.js
@@ -0,0 +1,6 @@
+// this is the bit of js that automatically scrolls the contents of the passage-container div to the bottom.
+
+// window.setInterval(function() {
+//     var elem = document.getElementById('passage-container');
+//     elem.scrollTop = elem.scrollHeight;
+//   }, 5000);
